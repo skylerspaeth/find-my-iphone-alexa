958e8b362b41670e327e5c7bf0a989b5c63a0741
Skyler Spaeth <skylerspaeth.scc@me.com>
2017-03-19 16:53:36 -0500

    updated gitignore

---
+++ example_config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    // Account information to authenticate with iCloud
+    username: 'user@domain.com',
+    password: 'P@55w0rd',
+
+    // Address to check distance of device from (no formatting characters)
+    address: '1234 Amazon Blvd Austin TX',
+
+    // Units when calculating distance: only 'km' or 'mi' for now
+    distanceUnits: 'km'
+};
